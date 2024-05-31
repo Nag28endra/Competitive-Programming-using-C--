424aab80df3bd5da3415e2451a8ca800cba8bc73
Nagendra <nagendramvb@gmail.com>
2024-05-31 23:27:55 +0530

    count the sub arrays with sum equals to K

--- count_subarrays_with_sumK.cpp
+++ count_subarrays_with_sumK.cpp
@@ -1,6 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int optimalApproach(vector<int> &arr, int n, int k){
+    int prefixSum = 0;
+    int cnt = 0;
+    map<int,int> mpp;
+    mpp[0] = 1;
+    for(int i=0;i<n;i++){
+        prefixSum += arr[i];
+        int rem = prefixSum - k;
+        cnt += mpp[rem];
+        mpp[prefixSum] += 1;
+    }
+    return cnt;
+}
 int betterApproach(vector<int> &arr,int n,int k){
     int cnt = 0;
 
@@ -21,5 +34,6 @@ int main()
 
     while(cin>>input && input!=EOF)arr.push_back(input);
     cout<<"Better:"<<betterApproach(arr,arr.size(),3);
+    cout<<"\nOptimal:"<<optimalApproach(arr,arr.size(),3);
     return 0;
 }
