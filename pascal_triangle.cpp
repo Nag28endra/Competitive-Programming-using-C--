74ab3e684892cb3521e3292aa3e87b83ddc21479
Nagendra <nagendramvb@gmail.com>
2024-06-01 22:49:42 +0530

    pascal triangle

--- pascal_triangle.cpp
+++ pascal_triangle.cpp
@@ -8,11 +8,22 @@ int printValue(int n, int r){
     }
     return res;
 }
+
+int printRow(int n){
+    for(int c = 1; c<=n;c++){
+        cout<<printValue(n-1,c-1)<<" ";
+    }
+    return 0;
+}
+
 int main()
 {
     int row,col;
+    int n;
     cin>>row>>col;
-    cout<<"Print the element at particular row and column\n";
-    cout<<printValue(row,col);
+    cin>>n;
+    // cout<<"Print the element at particular row and column\n";
+    // cout<<printValue(row,col);
+    printRow(n);
     return 0;
 }
