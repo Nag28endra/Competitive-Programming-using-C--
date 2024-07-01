2f5af608cacc0668df20c7edeb52c29828298a9b
Nagendra <nagendramvb@gmail.com>
2024-07-01 15:16:16 +0530

    linkedlist-creation, searching, length, traversal, deleting-head,tail,position

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -66,12 +66,75 @@ int search(Node* head, int target){
     }
     return 0;
 }
+Node* removeHead(Node* head){
+    if (head == NULL)return head;
+    Node* temp = head;
+    head = head->next;
+    //deleting the previous head from the heap.
+    delete temp;
+    return head;
+}
+Node* removeTail(Node* head){
+
+    //checking if the linked list is empty or it is having only one node
+    if(head == NULL || head->next == NULL) return NULL;
+
+    Node* temp = head;
+
+    while(temp->next->next != NULL){
+        temp = temp->next;
+    }
+    // deleting the last node called tail from the heap
+    free(temp->next);
+    temp->next = nullptr;
+    return head;
+}
+Node* removePos(Node* head,int k){
+    // if the linked list is null return the linked list.
+    if(head == NULL) return head;
+
+    // if the linked list is having single element return that.
+    if(k==1) return removeHead(head);
+    Node* temp = head;
+    Node* prev = NULL;
+    int cnt = 0;
+    while(temp){
+        cnt++;
+        if(cnt==k){
+            prev->next = prev->next->next;
+            free(temp);
+            break;
+        }
+        prev = temp;
+        temp = temp->next;
+    }
+    return head;
+}
+Node* removeValue(Node* head,int value){
+    // if the linked list is null return the linked list.
+    if(head == NULL) return head;
+
+    // if the linked list is having single element return that.
+    if(head->data==value) return removeHead(head);
+    Node* temp = head;
+    Node* prev = NULL;
+    
+    while(temp){
+        if(temp->data == value){
+            prev->next = prev->next->next;
+            free(temp);
+            break;
+        }
+        prev = temp;
+        temp = temp->next;
+    }
+    return head;
+}
 int main()
 {
     vector<int> arr{1,4,25,5};
     Node* head = convertArr2LL(arr);
-    cout<<"lenght of the linked list: "<<length(head)<<endl;
+    head = removeValue(head,5);
     traverse(head);
-    cout<<"\n"<<search(head,28);
     return 0;
 }
